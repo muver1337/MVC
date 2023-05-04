a8ce2337a27ca0ca57149b543ecab8b81f4d5256
Muver1337 <99253014+Muver1337@users.noreply.github.com>
2023-05-04 20:23:57 +0700

    "Не доделал функционал и одну страницу вёрстки"

--- views/site/discipline.php
+++ views/site/discipline.php
@@ -1,14 +1,33 @@
-
-<form class="forms2" method="post">
-     <h6> Дисциплины: </h6>
-    <select aria-label="Default select example">
-    <?php
-    foreach ($discipline as $dis) {
-        ?>
-        <option value="<?= $dis->discipline ?>"><?= $dis->discipline ?></option>
-    <?php }
-    ?>
-    </select>
-</form>
-
+<div class="alr">
+    <div class="left">
+        <form method="post">
+            <h6> Дисциплины: </h6>
+            <select class="forms2" aria-label="Default select example">
+                <?php
+                foreach ($discipline as $dis) {
+                    ?>
+                    <option value="<?= $dis->discipline ?>"><?= $dis->discipline ?></option>
+                <?php }
+                ?>
+            </select>
+        </form>
+        <div class="listwork">
+            <h6>Сотрудники</h6>
+            <div class="listback">
+                <?php
+                foreach ($worker as $wor) {
+                    ?>
+                    <li value="<?= $wor->id ?>"><?= $wor->discipline_id ?></li>
+                <?php }
+                ?>
+            </div>
+        </div>
+    </div>
+    <div class="right">
+        <h6 class="h66"> Добавить дисциплину: </h6>
+        <label>Новая дисциплина <br><input type="text" name="newdis"></label>
+        <label>Подразделение <br><input type="text" name="div"></label>
+        <button>Войти</button>
+    </div>
+</div>
 
--- views/site/division.php
+++ views/site/division.php
@@ -1,14 +1,44 @@
-
-<form class="forms" method="post">
-    <h6> Подразделения: </h6>
-    <select aria-label="Default select example">
-        <?php
-        foreach ($division as $div) {
-            ?>
-            <option value="<?= $div->division ?>"><?= $div->division ?></option>
-        <?php }
-        ?>
-    </select>
-</form>
-
+<div class="alr">
+    <div class="left">
+        <form method="post">
+            <h6> Подразделения: </h6>
+            <select aria-label="Default select example">
+                <?php
+                foreach ($division as $div) {
+                    ?>
+                    <option value="<?= $div->division ?>"><?= $div->discipline ?></option>
+                <?php }
+                ?>
+            </select>
+        </form>
+        <div class="listwork">
+            <p>Сотрудники</p>
+            <div class="listback">
+                <?php
+                foreach ($worker as $wor) {
+                    ?>
+                    <li value="<?= $wor->id ?>"><?= $wor->discipline_id ?></li>
+                <?php }
+                ?>
+            </div>
+        </div>
+    </div>
+    <div class="right">
+        <h6 class="newdiv"> Добавить подразделение: </h6>
+        <div>
+            <div>
+            <label>Название подразделения <br><input type="text" name="newdiv"></label> <button class="buttonn">Добавить</button>
+                <p>Дисциплины</p>
+                <div class="listback2">
+                    <?php
+                    foreach ($worker as $wor) {
+                        ?>
+                        <li value="<?= $wor->id ?>"><?= $wor->discipline_id ?></li>
+                    <?php }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
